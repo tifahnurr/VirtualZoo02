c7d179a1740beea11c828705193fdbf23e211499
Ega Rifqi Saputra <egarifqi@gmail.com>
2017-03-15 13:40:10 +0700

    Update habitat.h

--- habitat.h
+++ habitat.h
@@ -3,14 +3,32 @@
 #include <string>
 #include "cage.h"
 
+/**@class Habitat
+ * Kelas yang digunakan untuk membuat habitat */
 class Habitat {
   public:
-	Habitat();
+    /**@brief Constructor.
+     * Mengkonstruksi habitat tanpa parameter*/
+    Habitat();
+    /**@brief Constructor.
+     * Mengkonstruksi habitat dengan parameter c berupa tipe pembeda habitat
+     * @param c Tipe pembeda dari habitat*/
     Habitat(char c);
+    /**@brief SetCage.
+     * Menginisiasi cage dengan tipe pembeda dari habitat
+     * @param c Tipe pembeda dari habitat*/
     void SetCage(char c);
+    /**@brief GetCage.
+     * Mengambil cage yang dituju */
     Cage GetCage ();
+    /**@brief GetCode.
+     * Mengambil code dari habitat*/
     char GetCode();
+    /**@brief Interact.
+     * Berinteraksi dengan habitat*/
     void Interact();
+    /**@brief Render.
+     * Menampilkan tampilan yang harus ditampilkan */
     void Render();
 
   protected:
