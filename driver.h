9f158b21000aa95686d17bb7b3c9d49335de21bc
Ega Rifqi Saputra <egarifqi@gmail.com>
2017-03-15 13:43:26 +0700

    Update driver.h

--- driver.h
+++ driver.h
@@ -4,7 +4,8 @@
 #include "zoo.h"
 #include "point.h"
 
-
+/**@class Driver.
+ * Kelas yang berguna untuk menampilkan peta zoo*/
 class Driver{
   public:
     static void PrintMap(Zoo& z, Point& p);
