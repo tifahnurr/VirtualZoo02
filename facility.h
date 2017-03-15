051021a3336363a1442c197a4e1b9725b86a9c8a
Ega Rifqi Saputra <egarifqi@gmail.com>
2017-03-15 13:33:31 +0700

    Update facility.h

--- facility.h
+++ facility.h
@@ -4,12 +4,33 @@
 #include <string>
 #include <iostream>
 
+/**@class Cell
+ * Kelas yang digunakan untuk membentuk fasilitas*/
 class Facility {
   public:
+    /**@brief Constructor
+     * Mengkonstruksi fasilitas tanpa parameter
+     */
     Facility();
+  
+    /**@brief Constructor.
+     * Mengkonstruksi fasilitas dengan tipe tertentu yang diambil dari parameter c
+     */
     Facility(char c);
+  
+    /**@brief Render.
+     * Menampilkan tampilan yang harus ditampilkan
+     */
     void Render();
+  
+    /**@brief GetCode.
+     * Mengambil karakter pembeda dari fasilitas
+     */
     char GetCode();
+  
+    /**@brief Interact.
+     * Berinteraksi dengan fasilitas
+     */
     void Interact();
 
   protected:
